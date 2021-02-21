e40331bec31f3f1516f1426d8a75ead13d2a299c
jose <joseacevp@gmail.com>
2021-02-21 10:30:36 +0100

    Tarea5 Inicio Ejercicio 2 ejercicio1 puntos 1 y 2 ok

---
+++ TareasAD/Tema5/Tema5DB4o/src/Utilidades/CambioSecretario.java
@@ -0,0 +1,23 @@
+/*
+ * Ejercicio Realizado Para el Modulo Acceso a Datos
+ * Curso Desarrollo de aplicaciones Multiplataforma
+ * IES Juan Bosco
+ */
+package Utilidades;
+
+
+
+/**
+ *
+ * @author Jose Antonio Acevedo Paredes
+ */
+public class CambioSecretario {
+
+    /*    3.	Cambiar de secretario de un jefe indicado. El nombre del jefe a
+    modificar y los datos del nuevo secretario se indicarán por teclado.*/
+    public static void cambiar() {
+
+        
+    }
+
+}
