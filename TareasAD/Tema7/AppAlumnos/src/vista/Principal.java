fd8dbdeec25707ecc7e47ddd1c63da4d16a0d6d2
jose <joseacevp@gmail.com>
2021-05-03 11:39:15 +0200

    Acceso a datos finalizado tarea 7
    Entregado a espera de nota

--- TareasAD/Tema7/AppAlumnos/src/vista/Principal.java
+++ TareasAD/Tema7/AppAlumnos/src/vista/Principal.java
@@ -35,6 +35,7 @@ public class Principal extends javax.swing.JFrame {
         botonAnadirAlumno = new javax.swing.JButton();
         botonConsultarAlumno = new javax.swing.JButton();
         botonEliminar = new javax.swing.JButton();
+        panelPeronalizado2 = new PanelPersonalizado.PanelPeronalizado();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -68,11 +69,24 @@ public class Principal extends javax.swing.JFrame {
             }
         });
 
+        panelPeronalizado2.setRutaImagen(new java.io.File("C:\\Users\\josea\\Documents\\FP\\AD\\Tareas\\Tema7\\Tema7\\AppAlumnos\\Imagenes\\imagen.png"));
+
+        javax.swing.GroupLayout panelPeronalizado2Layout = new javax.swing.GroupLayout(panelPeronalizado2);
+        panelPeronalizado2.setLayout(panelPeronalizado2Layout);
+        panelPeronalizado2Layout.setHorizontalGroup(
+            panelPeronalizado2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        panelPeronalizado2Layout.setVerticalGroup(
+            panelPeronalizado2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout panelPeronalizado1Layout = new javax.swing.GroupLayout(panelPeronalizado1);
         panelPeronalizado1.setLayout(panelPeronalizado1Layout);
         panelPeronalizado1Layout.setHorizontalGroup(
             panelPeronalizado1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelPeronalizado1Layout.createSequentialGroup()
+            .addGroup(panelPeronalizado1Layout.createSequentialGroup()
                 .addGap(35, 35, 35)
                 .addComponent(botonVerAlumnos)
                 .addGap(18, 18, 18)
@@ -82,11 +96,17 @@ public class Principal extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(botonEliminar)
                 .addContainerGap(13, Short.MAX_VALUE))
+            .addGroup(panelPeronalizado1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(panelPeronalizado2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         panelPeronalizado1Layout.setVerticalGroup(
             panelPeronalizado1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelPeronalizado1Layout.createSequentialGroup()
-                .addContainerGap(102, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(panelPeronalizado2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(panelPeronalizado1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(botonAnadirAlumno)
                     .addComponent(botonVerAlumnos)
@@ -177,5 +197,6 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JButton botonEliminar;
     private javax.swing.JButton botonVerAlumnos;
     private PanelPersonalizado.PanelPeronalizado panelPeronalizado1;
+    private PanelPersonalizado.PanelPeronalizado panelPeronalizado2;
     // End of variables declaration//GEN-END:variables
 }
